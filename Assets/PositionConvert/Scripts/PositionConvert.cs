eb8ca21b5c5056788e8da5d232c41064f65dd496
JiaoChenYang <794676828@qq.com>
2019-05-07 10:52:31 +0800

    PositionConvert
    两相机之间 坐标转换

---
+++ Assets/PositionConvert/Scripts/PositionConvert.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PositionConvert : MonoBehaviour
+{
+
+    public Camera UICamera;
+    public Camera V3Camera;
+
+    public Transform Target;
+
+    public RectTransform RootRect;
+
+    private Vector2 m_ScreenPos;
+
+    private Vector2 m_LocalPos;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (UICamera == null || V3Camera == null)
+            return;
+
+        m_ScreenPos = RectTransformUtility.WorldToScreenPoint(V3Camera, Target.position);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(RootRect.root as RectTransform, m_ScreenPos, UICamera, out m_LocalPos);
+
+        RootRect.anchoredPosition = m_LocalPos;
+
+    }
+
+}
