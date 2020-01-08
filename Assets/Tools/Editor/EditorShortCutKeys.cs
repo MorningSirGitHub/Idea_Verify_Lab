58e1b407cdf3b93812f5ef92325c25a161232ee9
JiaoChenYang <794676828@qq.com>
2020-01-08 11:07:59 +0800

    no message

---
+++ Assets/Tools/Editor/EditorShortCutKeys.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class EditorShortCutKeys : ScriptableObject
+{
+
+    // 使用快捷键F5进入或退出Play模式
+    [MenuItem("Edit/Run _F5")]
+    static void PlayGame()
+    {
+        if (!Application.isPlaying)
+        {
+            EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), "", false);
+        }
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+}
