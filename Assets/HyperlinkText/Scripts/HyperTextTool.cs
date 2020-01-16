18d0319be4e2fde27afe8eb148e7bf1f42eb8d8e
JiaoChenYang <794676828@qq.com>
2020-01-16 19:08:29 +0800

    更改 HyperText Tools 格式

--- Assets/HyperlinkText/Scripts/HyperTextTool.cs
+++ Assets/HyperlinkText/Scripts/HyperTextTool.cs
@@ -17,7 +17,7 @@ namespace UnityEngine.UI
         ///     </para>
         ///         {表情名称|宽度|高度（可空，默认字体大小）#传递参数（可空，否则表情响应点击，作为超链接处理并传递参数）}
         /// </summary>
-        public const string Emoji = "{{0}{1}{2}}";
+        public const string Emoji = "{0}{1}{2}";
         /// <summary>
         /// 下划线，不响应点击
         /// </summary>
@@ -30,7 +30,7 @@ namespace UnityEngine.UI
         ///     </para>
         ///         {0x01（默认）#HTML色值（可空，下划线颜色，默认字体颜色）#传递参数（非空，响应点击并传递参数）=超链接显示内容（非空）}
         /// </summary>
-        public const string Link = "{0x01{0}{1}={2}}";
+        public const string Link = "0x01{0}{1}={2}";
         /// <summary>
         /// 自定义表情图片及是否响应点击    (目前响应点击无效，需要设置层级)
         ///     <para>
@@ -38,7 +38,7 @@ namespace UnityEngine.UI
         ///     </para>
         ///         {0x02（默认）|宽度|高度（可空，默认字体大小）#传递参数（可空，否则表情响应点击，作为超链接处理并传递参数）=自定义加载参数（非空，路径）}
         /// </summary>
-        public const string Custom = "{0x02{0}{1}={2}}";
+        public const string Custom = "0x02{0}{1}={2}";
         /// <summary>
         /// Prefab特效或者复杂表情    (目前响应点击无效，需要设置层级)
         ///     <para>
@@ -46,7 +46,7 @@ namespace UnityEngine.UI
         ///     </para>
         ///         {0x03（默认）|宽度|高度（可空，默认字体大小）#传递参数（可空，否则表情响应点击，作为超链接处理并传递参数）=自定义加载参数（非空，路径）}
         /// </summary>
-        public const string Effect = "{0x03{0}{1}={2}}";
+        public const string Effect = "0x03{0}{1}={2}";
 
         /*
         Usage:
@@ -59,6 +59,10 @@ namespace UnityEngine.UI
             测试{0x03|64=aoman}自定义加载特效
          */
 
+        private static string PackHyperFormat(string content)
+        {
+            return "{" + content + "}";
+        }
         public static string GetSize(float width = -1, float height = -1)
         {
             var format = string.Empty;
@@ -74,9 +78,9 @@ namespace UnityEngine.UI
             else
                 return string.Format(Color, htmlColor);
         }
-        public static string GetTransfer(string transfer = "")
+        public static string GetTransfer(string transfer = "", string content = "")
         {
-            if (string.IsNullOrEmpty(transfer))
+            if (string.IsNullOrEmpty(transfer) && string.IsNullOrEmpty(content))
                 return string.Empty;
             else
                 return string.Format(Click, transfer);
@@ -88,14 +92,16 @@ namespace UnityEngine.UI
 
             var emojiSize = GetSize(width, height);
             var emojiTransfer = GetTransfer(transfer);
-            return string.Format(Emoji, emojiName, emojiSize, emojiTransfer);
+            var format = string.Format(Emoji, emojiName, emojiSize, emojiTransfer);
+            return PackHyperFormat(format);
         }
         public static string GetUnderLine(string content = "", string htmlColor = "")
         {
             if (string.IsNullOrEmpty(content))
                 return string.Empty;
 
-            return string.Format(UnderLine, htmlColor, content);
+            var format = string.Format(UnderLine, htmlColor, content);
+            return PackHyperFormat(format);
         }
         public static string GetLink(string content = "", string htmlColor = "", string transfer = "")
         {
@@ -103,7 +109,9 @@ namespace UnityEngine.UI
                 return string.Empty;
 
             var color = GetColor(htmlColor);
-            return string.Format(Link, color, transfer, content);
+            var transferLink = GetTransfer(transfer, content);
+            var format = string.Format(Link, color, transferLink, content);
+            return PackHyperFormat(format);
         }
         public static string GetCustom(string path, string transfer = "", float width = -1, float height = -1)
         {
@@ -111,7 +119,9 @@ namespace UnityEngine.UI
                 return string.Empty;
 
             var size = GetSize(width, height);
-            return string.Format(Custom, size, transfer, path);
+            var transferLink = GetTransfer(transfer);
+            var format = string.Format(Custom, size, transferLink, path);
+            return PackHyperFormat(format);
         }
         public static string GetEffect(string path, string transfer = "", float width = -1, float height = -1)
         {
@@ -119,7 +129,9 @@ namespace UnityEngine.UI
                 return string.Empty;
 
             var size = GetSize(width, height);
-            return string.Format(Effect, size, transfer, path);
+            var transferLink = GetTransfer(transfer);
+            var format = string.Format(Effect, size, transferLink, path);
+            return PackHyperFormat(format);
         }
     }
 }
